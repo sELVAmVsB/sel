5855937690e80ee8259ab654618b344b89f45fbb
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-02-09 14:17:08 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,22 @@
+
+ 
+
+#include <stdio.h>
+ 
+void main()
+{
+  
+    printf("Enter the number \n");
+    scanf("%ld", &num);
+    temp = num;
+    while (num > 0)
+    {
+        digit = num % 10;
+        sum  = sum + digit;
+        num /= 10;
+    }
+    printf("Given number = %ld\n", temp);
+    printf("Sum of the digits %ld = %ld\n", temp, sum);
+    return 0 ;
+    }
+   
