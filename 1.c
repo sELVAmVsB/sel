679ab4bcd30a0e9d3d3650f587b7d46f8fc1fd3f
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-02-09 13:58:41 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) 
+{
+int a,b,c,d;
+sum=0;
+printf("Enter n and k value:");
+scanf("%d,%d",&a,&b);
+for(i=0;i<n;i++)
+{
+printf("%d\t",i);
+}
+for(j=0;j<k;j++)
+{
+sum=sum+j;
+}
+printf("%d",sum);
+return 0;
+}
