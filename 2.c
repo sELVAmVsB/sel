864d082220fa3713528c47989cc129ed1b89b947
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-02-09 14:07:59 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+{
+int num;
+
+sum=0
+if(num<=0):
+	print("enter a positive number")
+for i in range(1,num+1):
+	sum=sum+i
+print(sum);
+getch ();
+returb 0;
+}
