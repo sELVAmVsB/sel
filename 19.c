34521da62bfe608f3a75f30896bfb78469ef8ce5
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-02-26 16:04:38 +0530

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>x=raw_input()
+
+if x.isalpha():
+
+	print "it is alphabet"
+
+else:
+	
+        print "it is not alphabet"
+        getch ();
+        return 0;
+        }
