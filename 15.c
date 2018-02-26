60f1a83f23aeb3511f3c86c836f95fd9f44241a8
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-02-26 15:54:24 +0530

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,22 @@
+
+    #include<stdio.h>
+    #include<conio.h>
+    int main( )
+    {
+    int no=2,temp,rem,sum,count=0,a,b;
+ 
+ 
+    {
+    temp=no;
+    sum=0;
+    while(temp>0)
+
+    }
+    if(no==sum)
+    {
+    printf("\n%d", no);
+    count++;
+    }
+getch();
+return 0;
+   
