5ef671b6d23c8833d4c76e42e7e543fbdd07f6e6
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-09 13:20:26 +0530

    Create m.4.c

---
+++ m.4.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+	int n,k,i,a[10],count=0;
+	scanf("%d %d",&n,&k);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	    if(a[i]==k)
+	    {
+	        count++;
+	    }
+	}
+	if(count!=0)
+	{
+	   printf("%d is repeated %d times",k,count);
+	}
+	if(count==0)
+	{
+	   printf("%d is not present",k);
+	 }
+	return 0;
+  getch0;
+}
