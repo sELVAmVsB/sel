c20a0edb898c572222d2544aa826685e6ead9b5b
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-09 13:09:21 +0530

    Create m.1.c

---
+++ m.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+int count=0,i;
+char s[100];
+scanf("%d",&s);
+for(i=o;s[i]!=0;i++)
+{
+if(s[i]=" ")
+{
+  count++;
+ 
+  getch ();
+  return 0;
+  }}}
