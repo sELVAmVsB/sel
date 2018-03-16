773b100da1db2d7772b6e23625e94719503f9ccd
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-16 03:54:26 -0700

    Create q3.c

---
+++ q3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include<string.h>
+void main() {
+char a[1x=input().title()
+print(x)#include <stdio.h>
+
+#include<string.h>
+
+int main(void) 
+{
+	char str1[50],str2[50];
+	int n1,n2;
+	
+	scanf("%s",str1);
+	
+	scanf("%s",str2);
+	n1=strlen(str1);
+	n2=strlen(str2);
+	if(n1>n2)
+	{
+		printf("\n%s",str1);
+	}
+	else if(n2>n1)
+	{
+		printf("\n%s",str2);
+	}
+	else
+	{
+		printf("\n %s",str2);
+	}
+	return 0;
+}
+while(i<=n)
+{
+printf("%s\n",a);	
+i++;	
+}#include <stdio.h>
+#include<string.h>#103rd prb
+print(input().title())
+int main()
+{ 
+    char a[1#102th prb
+n=int(input())
+k=n
+while(n%2==0):
+    print(int(n/2))
+    n=n/2
+if(n==k):
+    print(n)
