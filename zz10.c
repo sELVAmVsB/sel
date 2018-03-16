e174579aeb950aff73027408fb62f3df18a3c839
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-16 03:46:30 -0700

    Create zz10.c

---
+++ zz10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+void main() {
+char a[1
+while(i<=n)
+{
+printf("%s\n",a);	
+i++;	
+}#include <stdio.h>
+#include<string.h>
+int main()
+{ 
+    char a[1#102th prb
+n=int(input())
+k=n
+while(n%2==0):
+    print(int(n/2))
+    n=n/2
+if(n==k):
+    print(n)
