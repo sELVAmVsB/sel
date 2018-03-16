c574c759c5b8fb46e988046a01a04e2d9016c2c1
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-16 04:04:20 -0700

    Create qc10.c

---
+++ qc10.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#includes2);
+include <stdio.include<stdio.h>
+int sum(int a,int b)
+{
+	int sum=0;
+	sum=a+b;
+	printf("%d",sum);
+	reh>
+int main() 
+{
+	int n;
+	scanf("%d",&n);
+    printf("%d",(n
+    
+    for(i = 0; #include<stdio.h>
+void main()
+{
+int a[50],i,n,min,max;
+printf("enter the no of elements");
+scanf("%d",&n);
+printf("enter array elements");
+for(i=0;i<n;i++)
+{
+scainclude <stdio.h>
+int main() 
+{
+	int n,sum=0,i,average;
+	scanf("%d",&n);
+	int a[1000];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);	
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+a[i];
+	}
+	average=sum/n;nf("%d",&a[i]);
+}
+min=a[0];
+max=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]<min)
+{
+min=a[i];
+}
+if(a[i]>max)
+{
+max=a[i];
+}
+}
+  printf("max = %d",max);
+   printf("\nmin = %d",min);
+}
+
+© 2018 GitHub, Inc.s1[i] != '\0'; ++i);
+
+    for(j = 0; s2[j] != '\0'; ++j, ++i)
+    {
+        s1[i] =#include<stdio.h>
+void main()
+{
+    int a=123,n;
+    while(a<0);
+    {
+        n=a%10;
+        printf("%d",n);
+    }
+   
+}
+
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+API
+Training
+Shop
+Blog
+ s2[j];
+    }
+
+    s1[i] = '\0';
+    printf("%s", s1);
+
+    return 0;tle()
+print(x)#include <stdio.h>
+
+#include<string.h>
+
+int main(void) 
+{
+	char str1[50],str2[50];
+	int n1,n2;
+	
+	scanf("%s",str1);
+	
+	scanf("%s",str2);
+	n1=strlen(str1);
+	n2=strlen(str2);
+	if(n1>n2)
+	{
+		printf("\n%s",str1);
+	}
+	else if(n2>n1)
+	{
+		printf("\n%s",str2);
+	}
+	else
+	{
+		printf("\n %s",str2);
+	}
+	return 0;
+}
+while(i<=n)
+{
+printf("%s\n",a);	
+i++;	
+}#include <stdio.h>
+#include<string.h>#103rd prb
+print(input().title())
+int main()
+{ 
+    char a[1#102th prb
+n=int(input())
+k=n
+while(n%2==0):
+    print(int(n/2))
+    n=n/2
+if(n==k):
+    print(n)
