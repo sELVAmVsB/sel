ed006674de788f9274668e6df288529bba145dc9
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-16 03:50:12 -0700

    Create q2.c

---
+++ q2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+void main() {
+char a[1x=input().title()
+print(x)
+while(i<=n)
+{
+printf("%s\n",a);	
+i++;	
+}#include <stdio.h>
+#include<string.h>#103rd prb
+print(input().title())
+int main()
+{ 
+    char a[1#102th prb
+n=int(input())
+k=n
+while(n%2==0):
+    print(int(n/2))
+    n=n/2
+if(n==k):
+    print(n)
