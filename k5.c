1589a37fb52903cec5c123876f58ab0c2739e7b8
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-16 04:13:59 -0700

    Create k5.c

---
+++ k5.c
@@ -0,0 +1,29 @@
+
+
+
+{
+}()scanf("%d",&n);
+	if(n<=0)
+
+}
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+API
+Training
+Shop
+Blog
+
+{ 
+    char a[1#102th prb
+n=int(input())
+k=n
+while(n%2==0):
+    print(int(n/2))
+    n=n/2
+if(n==k):
+    print(n)
