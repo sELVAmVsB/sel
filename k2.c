c19eaf4a319ccfd61ac260f6c52701752f3e713c
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-16 04:07:32 -0700

    Create k2.c

---
+++ k2.c
@@ -0,0 +1,58 @@
+
+
+
+{
+printf("%s\n",a);	
+i++;	
+}#include <stdio.h>
+#include<string.h>#103rd prb
+print(input().title())
+int main,a,p=1;
+printf("Enter the number and it's power");
+scanf("%d%d",&a,&n);
+for(i=0;i<n;i++)
+{
+p=p*a;
+}()scanf("%d",&n);
+	if(n<=0)
+	{
+		printf("enter only number greater than 0");
+		return(n);
+	}
+	if(n==1)
+	{
+		printf("yes");
+	}
+	if(n>1)
+	{
+		while(x<n)
+		{
+			x=x*2;
+		}
+		if(x==n)
+		printf("yes");
+		else
+		printf("no");
+	}
+}
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+API
+Training
+Shop
+Blog
+
+{ 
+    char a[1#102th prb
+n=int(input())
+k=n
+while(n%2==0):
+    print(int(n/2))
+    n=n/2
+if(n==k):
+    print(n)
