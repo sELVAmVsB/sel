930e41f694673e1b48a9632906b7a363121623a6
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-19 15:37:48 +0530

    Create qw.c

---
+++ qw.c
@@ -0,0 +1,16 @@
+#108th prb
+a1,b1,c1=map(int,).split(' '))
+n1=a11)*b1
+print(int(c1*(a1+n1)/2))#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,r;
+clrscr();
+printf("Enter the numbers:");
+scanf("%d%d%d",&a,&b,&c);
+r=a+(c-1)*b;
+printf("The result is:%d",r);
+getch();
+return 0;
+}
