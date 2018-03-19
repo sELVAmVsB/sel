e615c4bde180ea4f9849d401ae5399fdde49a26a
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-19 15:41:33 +0530

    Create ppp.c

---
+++ ppp.c
@@ -0,0 +1,47 @@
+#108th prb
+a1,b1,c1=map(int,).split(' '))
+n1=a11)*b1
+print(int(c1*(a1+n1)/2))#include<stdio.h>
+#include<conio.h>
+int main()#include<stdio.h>
+void main()
+{
+int k,sum=0,i;
+printf("Enter k value");
+scanf("%d",&k);
+for(i=1;i<=k;i++)
+{
+sum=sum+i;
+}
+printf("sum of %d natural numbers:%d",k,sum);
+}
+{
+int a,b,c,r;
+clrscr();
+printf("Enter the numbers:");
+scanf("%d%d%d",&a,&b,&c);
+r=a+(c-1)*b;#include <stdio.h>
+  int main() {
+    int a[10];
+    int i;
+    int smallest;
+    printf("Enter ten values:");
+    for (i = 0; i < 10; i++)
+    {
+    scanf("%d\n", &a[i]);
+    }
+    smallest = a[0];
+    for (i = 0; i < 10; i++)
+    {
+if (a[i] < smallest)
+{
+smallest = a[i];
+    }
+    }
+    printf("smallest of ten numbers is %d\n", smallest);
+    return 0;
+  }
+printf("The result is:%d",r);
+getch();
+return 0;
+}
