7f1ef690215ac642e6bb5cea650443f3f14db89d
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-26 16:15:14 +0530

    Create bhxdcfjsdcf.c

---
+++ bhxdcfjsdcf.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+void m#include<stdio.h>
+void main()
+{
+    int a,b,c=0,d;
+    printf("enter the numbers");
+    scanf("%d",&a);
+    while(a!=0)#92th prb
+ss=input()
+p1=0
+for i in range(len(ss)):
+    p1=p1+int(ss[i])
+print(p1)
+        c=c/10;
+        if(d%2!=0)
+        {
+            printf("%d",d);
+        }
+    }f the 2 numbers is %d",mul);
+ s=sqrt(mul);
+ if(m==n1)
+ {
+   if(m==n2)
+   {
+     printf("the number is a perfect square");
+     }
+     }
+  else
+  {
+   printf("the number is not a perfect square");
+   }
+   }
+ ntf("%c",s[i]);
+		}
+	}
+}
+if(n%2==0)
+{
+for(i=0;i<n;i++)
+	{
+		if(i==d-1)
+		{
+	            printf("*");
+		}
+		else if(i==d)
+            	{
+		printf("*");
+	            }
+		else
+		{
+			printf("%c",s[i]);
+		}
+	}	
+}
+
+
+
+	return 0;
