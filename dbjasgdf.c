28434183cbc86a625d070467b538952dc53b674a
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-26 16:16:15 +0530

    Create dbjasgdf.c

---
+++ dbjasgdf.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+void m#include<stdio.h>
+void main()
+{
+    int a,b,c=0,d;
+  
+ if(m==n1)
+ {
+   if(m==n2)
+   {
+     printf("the number is a perfect square");
+     }
+     }
+  else
+  {
+   printf("the number is not a perfect square");
+   }
+   }
+ ntf("%c",s[i]);
+		}
+	}
+}
+if(n%2==0)
+{
+for(i=0;i<n;i++)
+	{
+		if(i==d-1)
+		{
+	            printf("*");
+		}
+		else if(i==d)
+            	{
+		printf("*");
+	            }
+		else
+		{
+			printf("%c",s[i]);
+		}
+	}	
+}
+
+
+
+	return 0;
