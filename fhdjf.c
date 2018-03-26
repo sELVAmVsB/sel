102f35feb368d68a9b3ea1c0e22ab5ff0b2ca22d
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-26 16:02:38 +0530

    Create fhdjf.c

---
+++ fhdjf.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+void m#include<stdio.h>
+void main()
+{
+    int a,b,c=0,d;
+    printf("enter the numbers");
+    scanf("%d",&a);
+    while(a!=0)
+    {
+        b=a%10;
+        c=(c*10)+b;
+        a=a/10;
+    }
+    printf("the odd values are:");
+    while(c!=0)
+    {
+        d=c%10;
+        c=c/10;
+        if(d%2!=0)
+        {
+            printf("%d",d);
+        }
+    }f the 2 numbers is %d",mul);
+ s=sqrt(mul);
+ if(m==n1)
+ {
+   if(m==n2)
+   {
+     printf("the number is a perfect square");
+     }
+     }
+  else
+  {
+   printf("the number is not a perfect square");
+   }
+   }
+ ntf("%c",s[i]);
+		}
+	}
+}
+if(n%2==0)
+{
+for(i=0;i<n;i++)
+	{
+		if(i==d-1)
+		{
+	            printf("*");
+		}
+		else if(i==d)
+            	{
+		printf("*");
+	            }
+		else
+		{
+			printf("%c",s[i]);
+		}
+	}	
+}
+
+
+
+	return 0;
