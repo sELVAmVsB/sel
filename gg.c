84a422396d06ed0a0102810bf422bb5baea6e9f1
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-26 15:48:44 +0530

    Create gg.c

---
+++ gg.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+float n;
+int res;
+scanf("%f",&n);
+res=n+0.5;
+printf("%d",res);
+oid main()
+{
+int a;
+float b;
+printf("enter a number");
+scanf("%f",b);
+a=(n<0)?n-0.5:n+0.5;
+printf("%d",a);
+}
