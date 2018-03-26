d3d4e036d8ad04701503aafbb2e050d816eb64ab
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-26 15:55:21 +0530

    Create fsdkgfjkdnsdn.c

---
+++ fsdkgfjkdnsdn.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+void main()
+{
+{
+int a;
+float b;
+printf("enter a number");
+scanf("%f",b);
+a=(n<0)?n-0.5:n+0.5;
+printf("%d",a);
+}
+include<string.h>
+int main(void) 
+{oid main()
+{
+int num,flag=0,i;
+printf("\nEnter a number");
+scanf("%d",&num);
+for(i=2;i<(num/2);i++)
+{
+if(num%i==0)
+{
+flag=1;
+}
+}
+if(flag==1)
+{
+printf("\nYes");
+}
+else
+{
+printf("\nNo");"*");
+		}
+		else
+		{
+			printf("%c",s[i]);
+		}
+	}
+}
+if(n%2==0)
+{
+for(i=0;i<n;i++)
+	{
+		if(i==d-1)
+		{
+	            printf("*");
+		}
+		else if(i==d)
+            	{
+		printf("*");
+	            }
+		else
+		{
+			printf("%c",s[i]);
+		}
+	}	
+}
+
+
+
+	return 0;
