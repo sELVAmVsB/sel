7518a22cba2adb28ed16318d42eaeccd29b40aa6
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-26 15:57:44 +0530

    Create dhgfj.c

---
+++ dhgfj.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+void main()
+{
+{
+int a;
+float b;
+print{
+int a,i;
+scanf("%d",&a);
+printf("The factors are:");
+for(i=0;i<100;i++)
+{
+if(a%i==0)
+printf("%d",i);
+};i<(num/2);i++)
+{
+if(num%i==0)
+{
+flag=1;
+}
+}
+if(flag==1)
+{
+printf("\nYes");
+}
+else
+{
+printf("\nNo");"*");
+		}
+		else
+		{
+			printf("%c",s[i]);
+		}
+	}
+}
+if(n%2==0)
+{
+for(i=0;i<n;i++)
+	{
+		if(i==d-1)
+		{
+	            printf("*");
+		}
+		else if(i==d)
+            	{
+		printf("*");
+	            }
+		else
+		{
+			printf("%c",s[i]);
+		}
+	}	
+}
+
+
+
+	return 0;
