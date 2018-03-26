5c2934c712f8df28a875546b41b55dd51ed4cef3
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-26 15:59:27 +0530

    Create fsdfsdfh.c

---
+++ fsdfsdfh.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main()
+{
+{
+int a;
+float b;
+if(flag==1)
+{
+printf("\nYes");
+}
+else
+{
+printf("\nNo");"*");
+		}
+		else
+		{
+			printf("%c",s[i]);
+		}
+	}
+}
+if(n%2==0)
+{
+for(i=0;i<n;i++)
+	{
+		if(i==d-1)
+		{
+	            printf("*");
+		}
+		else if(i==d)
+            	{
+		printf("*");
+	            }
+		else
+		{
+			printf("%c",s[i]);
+		}
+	}	
+}
+
+
+
+	return 0;
