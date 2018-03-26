1e129a726d546d02b614dedc8d731319d9ccc199
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-26 16:01:19 +0530

    Create bnfjasfjbfj.c

---
+++ bnfjasfjbfj.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+void main()
+{
+{
+int a;
+float b;#include<stdio.h>
+#include<conio.h>
+main()
+{
+ int n1,n2,mul,s;
+ printf("enter number 1");
+ scanf("%d",&n1);
+ printf("enter number 2");
+ scanf("%d",&n2);
+ mul=n1*n2;
+ printf("the product of the 2 numbers is %d",mul);
+ s=sqrt(mul);
+ if(m==n1)
+ {
+   if(m==n2)
+   {
+     printf("the number is a perfect square");
+     }
+     }
+  else
+  {
+   printf("the number is not a perfect square");
+   }
+   }
+ ntf("%c",s[i]);
+		}
+	}
+}
+if(n%2==0)
+{
+for(i=0;i<n;i++)
+	{
+		if(i==d-1)
+		{
+	            printf("*");
+		}
+		else if(i==d)
+            	{
+		printf("*");
+	            }
+		else
+		{
+			printf("%c",s[i]);
+		}
+	}	
+}
+
+
+
+	return 0;
