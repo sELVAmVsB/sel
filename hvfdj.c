803aab269b07eed9b7611651fe3371ff402cfbb2
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-28 16:04:24 +0530

    Create hvfdj.c

---
+++ hvfdj.c
@@ -0,0 +1,23 @@
+ do
+Privacy
+Sog
+include<stdio.h>
+void main()
+{h>
+
+int main(void)
+{
+	int i;
+	char a[5];
+	i='A';
+	printf("the ascii values:%u",i);
+	return 0;
+}
+printf(include<stdio.h>
+void main()
+{
+float a,b,c;
+scanf("%f\t%f",&a,&b);
+c=a*b;
+printf("%f",c);
+}
