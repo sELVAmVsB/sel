0cd751cee489bba3e94c82ee4954170d45135783
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-28 16:09:34 +0530

    Create gfsdjgjsdrfj.c

---
+++ gfsdjgjsdrfj.c
@@ -0,0 +1,21 @@
+ do
+Privacy
+Sog
+include<stdio.h>
+void main()
+{h>
+
+int main(
+float a,b,c;
+scanf("%f\t%f",&a,&b);
+c=a*b;
+printf("%f",c);
+gfhgfjgfjgfgfhfshfd
+hf
+ygj
+h
+gfj
+jgjjkhkhkhkghk
+k
+hkhkhkhgkgh
+khk
